9c04faca3472b9bb886c2ddc74759a038a5120b2
Tieg <tiegosullivanpsnl@gmail.com>
2020-04-18 17:09:26 +0200

    hello world

---
+++ flaskblog.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+posts = [
+    {
+        'author': 'Tieg O',
+        'title': 'Post 0',
+        'content': 'Post 0 content',
+        'date_posted': 'April 18 2020'
+    },
+    {
+        'author': 'Erin O',
+        'title': 'Post 1',
+        'content': 'Post 1 content',
+        'date_posted': 'April 18 2020'
+    }
+]
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+if __name__ == "__main__":
+    app.run(debug=True)
